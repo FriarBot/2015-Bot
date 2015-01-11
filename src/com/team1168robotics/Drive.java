a4d8cf3fb89cb1805e6f2b545e57e5fdb658a49d
Jack Szipszky <jszipszky@gmail.com>
2015-01-10 21:09:45 -0500

    Added comments and slight revisions to Drive and Ultra

--- src/com/team1168robotics/Drive.java
+++ src/com/team1168robotics/Drive.java
@@ -35,6 +35,9 @@ public class Drive {
     private static Talon leftB;
     private static int driveSys;  
     
+    /**
+     * Initializes the data members
+     */
     public static void init(){
         rightF = new Talon(0);
         rightB = new Talon(1);
@@ -43,43 +46,26 @@ public class Drive {
         driveSys = 0;
     }
     
+    /**
+     * Sums the three states of robot movement (Forward/Backward, Rotate Left/Right, and Strafe Left/Right)
+     * then limits the values to be acceptable by the Talon set() method
+     * @param controller The Joystick object the Xbox controller is hooked up to
+     */    
     private static void mecanum(Joystick controller){
         double leftY = controller.getRawAxis(2);
         double rightX = controller.getRawAxis(4);
         double leftX = controller.getRawAxis(1);
         
-        if(leftY - rightX - leftX > 1) {
-            rightF.set(1);
-        } else if (leftY - rightX - leftX < -1){
-            rightF.set(-1);
-        } else {
-            rightF.set(leftY - rightX - leftX);
-        }
-        if(leftY - rightX + leftX > 1) {
-            rightB.set(1);
-        } else if(leftY - rightX + leftX < -1) {
-            rightB.set(-1);
-        } else {
-            rightB.set(leftY - rightX + leftX);
-        }
-        
-        if (leftY + rightX + leftX > 1) {
-            leftF.set(1);
-        } else if (leftY + rightX + leftX < -1) {
-            leftF.set(-1);
-        } else {
-            leftF.set(leftY + rightX + leftX);
-        }
-        
-        if (leftY + rightX - leftX > 1) {
-            leftB.set(1);
-        } else if (leftY + rightX - leftX < -1) {
-            leftB.set(-1);
-        } else {
-            leftB.set(leftY + rightX - leftX);
-        }
+        setRightF(leftY - rightX - leftX);
+        setRightB(leftY - rightX + leftX);
+        setLeftF(leftY + rightX + leftX);
+        setLeftB(leftY + rightX - leftX);        
     }
     
+    /**
+     * Simple tank drive
+     * @param controller The Joystick object the Xbox controller is hooked up to
+     */    
     private static void tank(Joystick controller) {
         double leftY = controller.getRawAxis(2);
         double rightY = controller.getRawAxis(5);
@@ -87,6 +73,11 @@ public class Drive {
         rightB.set(leftY);
     }
     
+    /**
+     * Switches between the tank and mecanum methods of driving
+     * Uses an int for expansion ability if more driving systems are added
+     * @param controller The Joystick object the Xbox controller is hooked up to
+     */
     public static void driveSys(Joystick controller){
         if(controller.getRawButton(1)){
             driveSys++;
@@ -97,11 +88,71 @@ public class Drive {
         driveOpt(controller);
     }
     
+    /**
+     * Determines which drive system to use based on the value of driveSys
+     * @param controller The Joystick object the Xbox controller is hooked up to
+     */
     private static void driveOpt(Joystick controller) {
         if(driveSys == 0){
             mecanum(controller);
         } else if (driveSys == 1) {
             tank(controller);
+        }
+    }
+    
+    /**
+     * Sets the rightF Talon controller
+     * @param rightFVal Raw Joystick input
+     */
+    private static void setRightF(double rightFVal){
+    	 if(rightFVal > 1) {
+             rightF.set(1);
+         } else if (rightFVal < -1){
+             rightF.set(-1);
+         } else {
+             rightF.set(rightFVal);
+         }
+    }
+    
+    /**
+     * Sets the rightB Talon motor controller
+     * @param rightBVal Raw Joystick input
+     */
+    private static void setRightB(double rightBVal){
+    	if(rightBVal > 1) {
+            rightB.set(1);
+        } else if(rightBVal < -1) {
+            rightB.set(-1);
+        } else {
+            rightB.set(rightBVal);
+        }
+    }
+    
+    /**
+     * Sets the leftF Talon motor controller
+     * @param leftFVal Raw Joystick input
+     */
+    private static void setLeftF(double leftFVal){
+    	if (leftFVal > 1) {
+            leftF.set(1);
+        } else if (leftFVal < -1) {
+            leftF.set(-1);
+        } else {
+            leftF.set(leftFVal);
+        }
+    }
+    
+    /**
+     * Sets the leftB Talon motor controller
+     * @param leftBVal Raw Joystick input
+     */
+    private static void setLeftB(double leftBVal){
+    	if (leftBVal > 1) {
+            leftB.set(1);
+        } else if (leftBVal < -1) {
+            leftB.set(-1);
+        } else {
+            leftB.set(leftBVal);
         }
     }
     
--- src/com/team1168robotics/Ultra.java
+++ src/com/team1168robotics/Ultra.java
@@ -1,11 +1,12 @@
 package com.team1168robotics;
 
 import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Ultra {
 	private Ultrasonic ultra = new Ultrasonic(9, 8, Ultrasonic.Unit.kInches);
 	
-	public void test(){
-		
+	public void dashboard(){
+		SmartDashboard.putNumber("Ultrasonic Range (inches):", ultra.getRangeInches());
 	}
 }
